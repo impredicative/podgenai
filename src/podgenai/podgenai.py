6c665e72d000c4cc7fef13e87da693a64cf7aa98
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-03-30 02:35:39 -0400

    Adjust pause order

--- src/podgenai/podgenai.py
+++ src/podgenai/podgenai.py
@@ -17,11 +17,11 @@ def generate_podcast(topic: str, *, output_path: Optional[Path] = None) -> Path:
     subtopics_list = list_subtopics(topic)  # Already validated.
     print(f'\nSUBTOPICS:\n{'\n'.join(subtopics_list)}')
     subtopics = {s: get_subtopic(topic=topic, subtopics=subtopics_list, subtopic=s) for s in subtopics_list}
-    subtopics = [f'{{pause}} Section {subtopic_name.replace('.', ':', 1)}:\n{subtopic_text}' for subtopic_name, subtopic_text in subtopics.items()]
+    subtopics = [f'Section {subtopic_name.replace('.', ':', 1)}:\n{subtopic_text} {{pause}}' for subtopic_name, subtopic_text in subtopics.items()]
 
     parts = subtopics.copy()
-    parts[0] = f'{topic}\n\n{parts[0]}'
-    parts[-1] = f'{parts[-1]}\n\n{{pause}} {TTS_DISCLAIMER}'
+    parts[0] = f'{topic}\n\n{{pause}} {parts[0]}'
+    parts[-1] = f'{parts[-1]}\n\n {TTS_DISCLAIMER}'
     text = '\n\n'.join(parts)
     print(f'\nTEXT:\n{text}')
 
@@ -29,10 +29,12 @@ def generate_podcast(topic: str, *, output_path: Optional[Path] = None) -> Path:
         now = datetime.datetime.now().isoformat(timespec='seconds')
         output_path = REPO_PATH / f'{now} {topic}.mp3'
 
+    part_paths = []
     for num, part in enumerate(parts):
         assert len(part) < 4096  # TODO: Split part across paragraphs if longer.
         part_path = output_path.with_name(f'{topic} - {subtopics_list[num]}.mp3')
-        if not part_path.exists():  # TODO: Use proper disk cache instead.
-            write_speech(part, part_path)
+        part_paths.append(part_path)
+        # if not part_path.exists():  # TODO: Use proper disk cache instead.
+        #     write_speech(part, part_path)
 
     return output_path
