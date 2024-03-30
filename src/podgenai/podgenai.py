0156bd4c623cf8294e38f1ecafb42d56d01fb6d1
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-03-30 02:14:27 -0400

    TTS parts

--- src/podgenai/podgenai.py
+++ src/podgenai/podgenai.py
@@ -17,14 +17,21 @@ def generate_podcast(topic: str, *, output_path: Optional[Path] = None) -> Path:
     subtopics_list = list_subtopics(topic)  # Already validated.
     print(f'\nSUBTOPICS:\n{'\n'.join(subtopics_list)}')
     subtopics = {s: get_subtopic(topic=topic, subtopics=subtopics_list, subtopic=s) for s in subtopics_list}
-    subtopics = [f'{{pause}} SECTION {subtopic_name.replace('.', ':', 1)}:\n{subtopic_text}' for subtopic_name, subtopic_text in subtopics.items()]
+    subtopics = [f'{{pause}} Section {subtopic_name.replace('.', ':', 1)}:\n{subtopic_text}' for subtopic_name, subtopic_text in subtopics.items()]
 
-    text = f'{topic}\n\n{'\n\n'.join(subtopics)}\n\n{{pause}} {TTS_DISCLAIMER}'
+    parts = subtopics.copy()
+    parts[0] = f'{topic}\n\n{parts[0]}'
+    parts[-1] = f'{parts[-1]}\n\n{{pause}} {TTS_DISCLAIMER}'
+    text = '\n\n'.join(parts)
     print(f'\nTEXT:\n{text}')
 
     if output_path is None:
         now = datetime.datetime.now().isoformat(timespec='seconds')
         output_path = REPO_PATH / f'{now} {topic}.mp3'
-    # write_speech(text, output_path)
+
+    for num, part in enumerate(parts):
+        assert len(part) < 4096  # TODO: Split part across paragraphs if longer.
+        part_path = output_path.with_name(f'{output_path.stem} - {subtopics_list[num]}.mp3')
+        write_speech(part, part_path)
 
     return output_path
