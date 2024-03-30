0920d122ad4ea2d89e82d9d50da65dd7fc428fc1
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-03-29 21:34:54 -0400

    Get topic

--- src/podgenai/__main__.py
+++ src/podgenai/__main__.py
@@ -1,6 +1,14 @@
+def get_topic() -> str:
+    topic = input('Specify the topic for which to generate a podcast: ')
+    topic = topic.strip()
+    if len(topic) < 2:
+        raise ValueError('Invalid topic.')
+    return topic
+
+
 def main() -> None:
-    import sys
-    print(f"Hello from podgenai using {sys.version}!")
+    topic = get_topic()
+    print(f'Generating podcast on: {topic}')
 
 
 if __name__ == "__main__":
