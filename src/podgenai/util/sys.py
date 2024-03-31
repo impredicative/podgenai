e3e964d943813388eda8f955130e6c3bd9df1ddc
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-03-31 19:57:09 -0400

    Add function print_warning

--- src/podgenai/util/sys.py
+++ src/podgenai/util/sys.py
@@ -3,3 +3,7 @@ import sys
 
 def print_error(error: str) -> None:
     print(f'Error: {error}', file=sys.stderr)
+
+
+def print_warning(warning: str) -> None:
+    print(f'Warning: {warning}', file=sys.stderr)
