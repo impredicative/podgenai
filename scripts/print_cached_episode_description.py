8f5f03a0f86c50aa041146c1ad5f0db8f967c420
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-04-20 13:00:38 -0400

    Update script which generates episode description

--- scripts/print_cached_episode_description.py
+++ scripts/print_cached_episode_description.py
@@ -7,7 +7,7 @@ def _get_denumbered_subsections(lines: list[str]) -> list[str]:
     return [line[line.find(" ") + 1 :] if line.find(".") != -1 else line for line in lines]
 
 
-def get_cached_episode_description_html(topic: str, fmt: str = "html") -> str:
+def get_cached_episode_description_html(topic: str, fmt: str = "plain") -> str:
     work_path = get_topic_work_path(topic, create=False)
     if not work_path.is_dir():
         raise LookupError(f"Work path does not exist for topic: {topic}")
@@ -21,17 +21,17 @@ def get_cached_episode_description_html(topic: str, fmt: str = "html") -> str:
         raise LookupError(f"Multiple {num_subtopics_list_files} subtopic lists exist for topic: {topic}:\n{subtopics_list_files_str}")
     assert num_subtopics_list_files == 1
     subtopics_list_file = subtopics_list_files[0]
-    subtopics_list = subtopics_list_file.read_text().strip().split("\n")
+    subtopics_text = subtopics_list_file.read_text().strip()
+    subtopics_list = subtopics_text.split("\n")
     assert subtopics_list
 
     match fmt:
         case "html":
-            subtopics_list = _get_denumbered_subsections(subtopics_list)
-            subtopics_list_html = "\n".join(f"  <li>{s}</li>" for s in subtopics_list)
+            denumbered_subtopics_list = _get_denumbered_subsections(subtopics_list)
+            subtopics_list_html = "\n".join(f"  <li>{s}</li>" for s in denumbered_subtopics_list)
             description = f"<p><strong>Sections</strong>:</p>\n<ol>\n{subtopics_list_html}\n</ol>\n<p><br></p><p><strong>Disclaimer</strong>: <em>{PROMPTS['tts_disclaimer']}</em></p>"
         case "plain":
-            subtopics_list_markdown = "\n".join(subtopics_list)
-            description = f"Sections:\n\n{subtopics_list_markdown}"
+            description = f"Sections:\n\n{subtopics_text}"
         case _:
             assert False, fmt
 
