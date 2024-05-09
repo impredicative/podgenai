a8fb35cf78512cee3e512eebafa84a286f027422
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-05-08 23:50:10 -0400

    Update requirements

--- src/podgenai/util/semantic_text_splitter.py
+++ src/podgenai/util/semantic_text_splitter.py
@@ -4,6 +4,6 @@ from semantic_text_splitter import TextSplitter
 def semantic_split(text: str, limit: int) -> list[str]:
     """Return a list of chunks from the given text, splitting it at semantically sensible boundaries while applying the specified character length limit for each chunk."""
     # Ref: https://stackoverflow.com/a/78288960/
-    splitter = TextSplitter()
-    chunks = splitter.chunks(text, limit)
+    splitter = TextSplitter(limit)
+    chunks = splitter.chunks(text)
     return chunks
