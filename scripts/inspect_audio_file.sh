ac1baa68e7ac67864e99acaa03e421143d4a6c46
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2026-02-21 19:36:38 +0000

    Add sample format and time base to audio stream info output

--- scripts/inspect_audio_file.sh
+++ scripts/inspect_audio_file.sh
@@ -25,6 +25,6 @@ fi
 # Print audio stream info
 ffprobe -v error \
   -select_streams a:0 \
-  -show_entries stream=codec_name,codec_long_name,profile,sample_rate,channels,channel_layout,bit_rate \
+  -show_entries stream=codec_name,codec_long_name,profile,sample_rate,channels,channel_layout,bit_rate,sample_fmt,time_base \
   -of default=noprint_wrappers=1 \
   "$FILE"
