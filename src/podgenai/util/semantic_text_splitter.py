375caae8be4b5d7e2c9d11cfe86bf25d86b9b801
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2026-08-19 04:52:37 +0000

    Update semantic_text_splitter utility module with new function semantic_split_by_tokens

--- src/podgenai/util/semantic_text_splitter.py
+++ src/podgenai/util/semantic_text_splitter.py
@@ -1,9 +1,15 @@
 from semantic_text_splitter import TextSplitter
 
 
-def semantic_split(text: str, limit: int) -> list[str]:
+def semantic_split_by_length(text: str, limit: int) -> list[str]:
     """Return a list of chunks from the given text, splitting it at semantically sensible boundaries while applying the specified character length limit for each chunk."""
     # Ref: https://stackoverflow.com/a/78288960/
     splitter = TextSplitter(limit)
+    chunks = splitter.chunks(text)
+    return chunks
+
+def semantic_split_by_tokens(text: str, *, model: str, limit: int) -> list[str]:
+    """Return a list of chunks from the given text, splitting it at semantically sensible boundaries while applying the specified model's token length limit for each chunk."""
+    splitter = TextSplitter.from_tiktoken_model(model, limit)
     chunks = splitter.chunks(text)
     return chunks
