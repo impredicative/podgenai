4343f7596f7ace57a2bab3ba711d1a17d5625f85
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2026-08-21 04:14:04 +0000

    Address ruff issues

--- src/podgenai/config.py
+++ src/podgenai/config.py
@@ -12,7 +12,7 @@ REPO_PATH: Path = PACKAGE_PATH.parent.parent
 
 AUDIO_PATHS: dict[str, Path] = {p.stem: p for p in (PACKAGE_PATH / "audio").glob("*.mp3")}
 GiB = 1024**3
-MAX_CONCURRENT_WORKERS = int(os.environ.get("PODGENAI_OPENAI_MAX_WORKERS", 16))  # Note: Default value is documented in readme.
+MAX_CONCURRENT_WORKERS = int(os.environ.get("PODGENAI_OPENAI_MAX_WORKERS", str(16)))  # Note: Default value is documented in readme.
 assert MAX_CONCURRENT_WORKERS >= 1
 NUM_SECTIONS_MIN: int = 3  # Applies only to the `max_sections` argument. Does not apply to LLM output.
 NUM_SECTIONS_MAX: int = 100
--- src/podgenai/util/input.py
+++ src/podgenai/util/input.py
@@ -1,4 +1,3 @@
-
 import podgenai.exceptions
 
 
