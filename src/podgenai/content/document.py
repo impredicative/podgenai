f7b021fb40c2383594e7a55ef688b983c785b69d
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2026-08-29 19:29:40 +0000

    Update ensure_document_is_valid function to ensure that document tags are not present in document

--- src/podgenai/content/document.py
+++ src/podgenai/content/document.py
@@ -1,3 +1,5 @@
+import io
+
 import podgenai.exceptions
 from podgenai.util.openai import MODELS
 from podgenai.util.tiktoken import get_token_count
@@ -6,12 +8,13 @@ from podgenai.util.tiktoken import get_token_count
 def ensure_document_is_valid(document: str) -> None:
     """Raise `InputError` if the document is too large for the knowledge model or text model's input capacity."""
     assert isinstance(document, str), (document, type(document))
+    exception = podgenai.exceptions.InputError
 
     if document != document.strip():
-        raise podgenai.exceptions.InputError("Document contains leading or trailing whitespace.")
+        raise exception("Document contains leading or trailing whitespace.")
 
     if len(document) == 0:
-        raise podgenai.exceptions.InputError("Document is empty.")
+        raise exception("Document is empty.")
 
     models = (MODELS["knowledge"], MODELS["text"])
     for model in models:
@@ -20,4 +23,10 @@ def ensure_document_is_valid(document: str) -> None:
         if document_token_count > text_model_input_token_capacity:
             excess_tokens = document_token_count - text_model_input_token_capacity
             reduction_ratio = excess_tokens / document_token_count
-            raise podgenai.exceptions.InputError(f"Document uses {document_token_count:,} tokens, but the {model['name']} model's input capacity is {text_model_input_token_capacity:,} tokens. Reduce the document size by at least {excess_tokens:,} tokens ({reduction_ratio:.0%}).")
+            raise exception(f"Document uses {document_token_count:,} tokens, but the {model['name']} model's input capacity is {text_model_input_token_capacity:,} tokens. Reduce the document size by at least {excess_tokens:,} tokens ({reduction_ratio:.0%}).")
+
+    for line_num, line in enumerate(io.StringIO(document), start=1):
+        line = line.strip()
+        for tag in ("<document>", "</document>"):
+            if line == tag:
+                raise exception(f"Document contains a line with the '{tag}' tag on line {line_num}. It must not contain the tag.")
